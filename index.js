99f0b7819a9accee53061c2ebf5c79b5c56bc5d3
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-03-28 17:26:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = express();
+const port = process.env.PORT || 8800;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+// console.dir(app); // find out all that make an express app
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+});
+
+const Product = mongoose.model("Product", {
+  title: String,
+  price: Number,
+  description: String,
+});
+
+app.get("/products", (req, res) => {
+  Product.find().then((products) => res.json(products));
+});
+
+app.post("/products", (req, res) => {
+  Product.create(req.body).then((product) => res.json(product));
+});
+
+app.get("/products/:id", (req, res) => {
+  Product.findById(req.params.id).then((product) => res.json(product));
+});
+
+app.put("/products/:id", (req, res) => {
+  Product.findByIdAndUpdate(req.params.id, req.body).then(() => {
+    Product.findById(req.params.id).then((product) => res.json(product));
+  });
+});
+
+app.delete("/products/:id", (req, res) => {
+  Product.findByIdAndDelete(req.params.id).then(() => res.sendStatus(200));
+});
+
+app.use("*", (req, res) => {
+  res.send("404, page not found");
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
