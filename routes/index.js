cc7e556f32b73e4dd308346e7392c16be42c078a
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-04-02 17:05:14 +0100

    added demo template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.status(200).render("pages/index", { title: "Express" });
+  res
+    .status(200)
+    .render("pages/index", {
+      title: "Tolulope Title",
+      message: "Tolulope Message",
+      name: "Tolulope Name",
+    });
 });
 
 module.exports = router;
