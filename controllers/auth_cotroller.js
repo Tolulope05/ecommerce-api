4a0d57c3998e659ace95039990608c9c4266a2be
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-04-22 04:34:05 +0100

    verify auth controller

---
+++ controllers/auth_cotroller.js
@@ -0,0 +1,14 @@
+const auth = (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) return res.status(401).send("Access Denied");
+  try {
+    // verify token
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).send("Invalid Token Not authorized");
+  }
+};
+
+module.exports = auth;
