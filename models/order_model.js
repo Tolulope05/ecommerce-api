b2adf5876ffda57d6a143f4541a8e9ed829dff3c
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-05-02 06:33:11 +0100

    order

---
+++ models/order_model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: { type: Number, required: true },
+    },
+  ],
+  totalPrice: { type: Number, required: true },
+  status: {
+    type: String,
+    required: true,
+    enum: ["pending", "completed", "cancelled"],
+    default: "pending",
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
+
+// Path: routes\order.js
