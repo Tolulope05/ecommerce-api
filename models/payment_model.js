f4ab9da6caf251f826af14ad9bcdf83c05734c1c
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-05-03 04:01:17 +0100

    payment model

---
+++ models/payment_model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    paymentResult: {
+      id: String,
+      status: String,
+      update_time: String,
+      email_address: String,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    reference: {
+      type: String,
+      required: true,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Payment = mongoose.model("Payment", paymentSchema);
+
+module.exports = Payment;
