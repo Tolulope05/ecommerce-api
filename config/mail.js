4876a5375d0a2fd6c294a0e29c37bfbd4f045f9b
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-05-09 09:10:01 +0100

    sending mail

--- config/mail.js
+++ config/mail.js
@@ -25,5 +25,62 @@ const sendEmail = async (email, subject, html) => {
       console.log(err);
     }
     console.log("Email sent: " + info.response);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   });
+};
+
+const sendResetPasswordEmail = async (email, token) => {
+  const subject = "Reset Password";
+  const html = `
+    <p>Click this <a href="http://localhost:3000/reset-password/${token}">link</a> to reset your password</p>
+    `;
+  await sendEmail(email, subject, html);
+};
+
+const sendVerificationEmail = async (email, token) => {
+  const subject = "Verify Email";
+  const html = `
+        <p>Click this <a href="http://localhost:3000/verify-email/${token}">link</a> to verify your email</p>
+    `;
+  await sendEmail(email, subject, html);
+};
+
+const sendOrderEmail = async (email, order) => {
+  const subject = "Order Confirmation";
+  const html = `
+        <p>Thank you for your order</p>
+        <p>Order ID: ${order._id}</p>
+        <p>Order Items:</p>
+        <ul>
+            ${order.orderItems.map(
+              (item) =>
+                `<li>
+                    <img src="${item.image}" alt="${item.name}" />
+                    <div>
+                        <p>${item.name}</p>
+                        <p>Qty: ${item.quantity}</p>
+                        <p>Price: $${item.price}</p>
+                    </div>
+                </li>`
+            )}
+        </ul>
+        <p>Order Total: $${order.totalPrice}</p>
+    `;
+  await sendEmail(email, subject, html);
+};
+
+const sendForgotPasswordEmail = async (email, token) => {
+  const subject = "Reset Password";
+  const html = `
+        <p>Click this <a href="http://localhost:3000/reset-password/${token}">link</a> to reset your password</p>
+    `;
+  await sendEmail(email, subject, html);
+};
+
+module.exports = {
+  sendEmail,
+  sendResetPasswordEmail,
+  sendVerificationEmail,
+  sendOrderEmail,
+  sendForgotPasswordEmail,
 };
